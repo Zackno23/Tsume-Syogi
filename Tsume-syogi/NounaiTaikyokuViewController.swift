4d96d6f0bb679091950d806e4e8f03a1439693df
Chikara Yoshida <chikara.f.yoshida@gmail.com>
2019-06-22 13:34:59 +0900

    alamofire いれた?

--- Tsume-syogi/NounaiTaikyokuViewController.swift
+++ Tsume-syogi/NounaiTaikyokuViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class NounaiTaikyokuViewController: UIViewController {
-
+    @IBOutlet weak var Funyanza: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
